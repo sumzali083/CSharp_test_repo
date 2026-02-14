c83d484f69c11c787180a1c9b52e43c8f69e196a
sumzali083 <alisummer066@icloud.com>
2026-02-14 23:46:21 +0000

    Initial commit with gitignore

--- obj/Debug/net10.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net10.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ae80428a228c303c5716be5b32c46edc2c7e7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb8839a93ee476c729557cc2c8880d6aed730ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
